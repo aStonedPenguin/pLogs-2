0e4cd696d0a5a80fcbc3c785769f5f22dea470a0
aStonedPenguin <zach@superiorservers.co>
2016-01-08 14:11:04 -0900

    quick Maestro logs fix

--- lua/plogs_hooks/maestro.lua
+++ lua/plogs_hooks/maestro.lua
@@ -1,8 +1,19 @@
 plogs.Register('Maestro', false)
 
+local function concat(t)
+	local s = ''
+	for k, v in pairs(t) do
+		if (not istable(v)) then
+			s = s .. tostring(v)
+		else
+			s = s .. concat(v)
+		end
+	end
+end
+
 plogs.AddHook('maestro_command', function(pl, cmd, args)
 	if pl:IsPlayer() then 
-		plogs.PlayerLog(pl, 'Maestro', pl:NameID() .. ' has ran command "' .. cmd .. '" with args "' .. table.concat(args, ' ') .. '"', {
+		plogs.PlayerLog(pl, 'Maestro', pl:NameID() .. ' has ran command "' .. cmd .. '" with args "' .. concat(args, ' ') .. '"', {
 			['Name']	= pl:Name(),
 			['SteamID']	= pl:SteamID(),
 		})
